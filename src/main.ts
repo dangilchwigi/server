21f8f8248ffd155bc2bf497c0e94af65f00a8328
sinyeongseok <sinyeongseok99@gmail.com>
2023-07-03 01:28:34 +0900

    feat: cookieParser 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
+
   await app.listen(process.env.PORT || 8081);
 }
 bootstrap();
