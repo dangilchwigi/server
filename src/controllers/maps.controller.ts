a812f40b21e1b7bce3b14c12fde79b94c4821ae6
sinyeongseok <sinyeongseok99@gmail.com>
2023-07-28 23:44:34 +0900

    feat: kakao maps api를 이용한 서울 내 매장 검색 api 추가

--- src/controllers/maps.controller.ts
+++ src/controllers/maps.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { MapService } from 'src/providers/maps.service';
 
 @Controller('maps')
 export class MapController {
   constructor(private mapService: MapService) {}
+
+  @Get()
+  async searchStoreByName(@Res() res, @Query('keyword') keyword: string) {
+    const result = await this.mapService.searchStoreByName(keyword);
+
+    return res.status(result.statusCode).json(result.data);
+  }
 }
--- src/providers/maps.service.ts
+++ src/providers/maps.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class MapService {}
+export class MapService {
+  async searchStoreByName(name: string) {
+    try {
+      const response = await axios.get(
+        `https://dapi.kakao.com/v2/local/search/keyword.json?query=서울 ${encodeURIComponent(
+          name
+        )}`,
+        {
+          headers: {
+            Authorization: `KakaoAK ${process.env.KAKAO_REST_API_KEY}`,
+          },
+        }
+      );
+
+      const result = response.data.documents.map((document) => {
+        return {
+          placeName: document.place_name,
+          addressName: document.address_name,
+        };
+      });
+
+      return { statusCode: 200, data: { placeList: result } };
+    } catch (error) {
+      console.log(error);
+      return { statusCode: 500, data: '서버요청 실패.' };
+    }
+  }
+}
